e4435fb13d144885938a18d8a668110367a20d12
CiCi <cc03102005@gmail.com>
2025-05-01 23:04:25 +0700

    chore(repo): add the generated schema.sql

---
+++ repo/schema.sql
@@ -0,0 +1,512 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: accounts; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.accounts (
+    acc_id integer NOT NULL,
+    citizen_id character(12) NOT NULL,
+    username character varying(20) NOT NULL,
+    password_hash character varying(20) NOT NULL,
+    phone character(10) NOT NULL,
+    email text,
+    role character varying(10) NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: accounts_acc_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.accounts_acc_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: accounts_acc_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.accounts_acc_id_seq OWNED BY public.accounts.acc_id;
+
+
+--
+-- Name: messages; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.messages (
+    from_acc_id integer NOT NULL,
+    to_acc_id integer NOT NULL,
+    content text NOT NULL,
+    sent_at timestamp with time zone NOT NULL
+);
+
+
+--
+-- Name: patients; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.patients (
+    patient_id integer NOT NULL,
+    acc_id integer NOT NULL,
+    full_name character varying(50) NOT NULL,
+    date_of_birth date NOT NULL,
+    gender character varying(3) NOT NULL,
+    ethnicity character varying(15) NOT NULL,
+    nationality character varying(30),
+    address text NOT NULL,
+    health_insurance_expired_date date,
+    health_insurance_number character(15),
+    emergency_contact_info character varying NOT NULL
+);
+
+
+--
+-- Name: patients_patient_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.patients_patient_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: patients_patient_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.patients_patient_id_seq OWNED BY public.patients.patient_id;
+
+
+--
+-- Name: prescription_details; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.prescription_details (
+    detail_id integer NOT NULL,
+    prescription_id integer NOT NULL,
+    medication_name text NOT NULL,
+    dosage integer NOT NULL,
+    dosage_unit character varying(20) NOT NULL,
+    duration_days integer NOT NULL,
+    quantity integer NOT NULL,
+    frequency text NOT NULL,
+    instructions text NOT NULL
+);
+
+
+--
+-- Name: prescription_details_detail_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.prescription_details_detail_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: prescription_details_detail_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.prescription_details_detail_id_seq OWNED BY public.prescription_details.detail_id;
+
+
+--
+-- Name: prescription_details_prescription_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.prescription_details_prescription_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: prescription_details_prescription_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.prescription_details_prescription_id_seq OWNED BY public.prescription_details.prescription_id;
+
+
+--
+-- Name: prescriptions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.prescriptions (
+    prescription_id integer NOT NULL,
+    record_id integer NOT NULL,
+    is_insurance_covered boolean NOT NULL,
+    prescription_note text,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    received_at timestamp with time zone
+);
+
+
+--
+-- Name: prescriptions_prescription_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.prescriptions_prescription_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: prescriptions_prescription_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.prescriptions_prescription_id_seq OWNED BY public.prescriptions.prescription_id;
+
+
+--
+-- Name: records; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.records (
+    record_id integer NOT NULL,
+    patient_id integer NOT NULL,
+    doctor_id integer NOT NULL,
+    type text NOT NULL,
+    main_diagnosis text,
+    secondary_diagnosis text,
+    record_detail_path text NOT NULL,
+    discharged_at timestamp with time zone,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    expired_at timestamp with time zone NOT NULL
+);
+
+
+--
+-- Name: records_record_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.records_record_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: records_record_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.records_record_id_seq OWNED BY public.records.record_id;
+
+
+--
+-- Name: schedules; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.schedules (
+    schedule_id integer NOT NULL,
+    patient_id integer NOT NULL,
+    queue_number integer NOT NULL,
+    examination_date date NOT NULL,
+    expected_examination_time time with time zone NOT NULL,
+    status character varying(30) NOT NULL
+);
+
+
+--
+-- Name: schedules_schedule_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.schedules_schedule_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: schedules_schedule_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.schedules_schedule_id_seq OWNED BY public.schedules.schedule_id;
+
+
+--
+-- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.schema_migrations (
+    version character varying(128) NOT NULL
+);
+
+
+--
+-- Name: staffs; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.staffs (
+    staff_id integer NOT NULL,
+    acc_id integer NOT NULL,
+    full_name text NOT NULL,
+    date_of_birth date NOT NULL,
+    gender character varying(3) NOT NULL,
+    department text NOT NULL
+);
+
+
+--
+-- Name: staffs_staff_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.staffs_staff_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: staffs_staff_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.staffs_staff_id_seq OWNED BY public.staffs.staff_id;
+
+
+--
+-- Name: accounts acc_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.accounts ALTER COLUMN acc_id SET DEFAULT nextval('public.accounts_acc_id_seq'::regclass);
+
+
+--
+-- Name: patients patient_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.patients ALTER COLUMN patient_id SET DEFAULT nextval('public.patients_patient_id_seq'::regclass);
+
+
+--
+-- Name: prescription_details detail_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prescription_details ALTER COLUMN detail_id SET DEFAULT nextval('public.prescription_details_detail_id_seq'::regclass);
+
+
+--
+-- Name: prescription_details prescription_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prescription_details ALTER COLUMN prescription_id SET DEFAULT nextval('public.prescription_details_prescription_id_seq'::regclass);
+
+
+--
+-- Name: prescriptions prescription_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prescriptions ALTER COLUMN prescription_id SET DEFAULT nextval('public.prescriptions_prescription_id_seq'::regclass);
+
+
+--
+-- Name: records record_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.records ALTER COLUMN record_id SET DEFAULT nextval('public.records_record_id_seq'::regclass);
+
+
+--
+-- Name: schedules schedule_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schedules ALTER COLUMN schedule_id SET DEFAULT nextval('public.schedules_schedule_id_seq'::regclass);
+
+
+--
+-- Name: staffs staff_id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.staffs ALTER COLUMN staff_id SET DEFAULT nextval('public.staffs_staff_id_seq'::regclass);
+
+
+--
+-- Name: accounts accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (acc_id);
+
+
+--
+-- Name: patients patients_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.patients
+    ADD CONSTRAINT patients_pkey PRIMARY KEY (patient_id);
+
+
+--
+-- Name: prescription_details prescription_details_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prescription_details
+    ADD CONSTRAINT prescription_details_pkey PRIMARY KEY (detail_id);
+
+
+--
+-- Name: prescriptions prescriptions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prescriptions
+    ADD CONSTRAINT prescriptions_pkey PRIMARY KEY (prescription_id);
+
+
+--
+-- Name: records records_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.records
+    ADD CONSTRAINT records_pkey PRIMARY KEY (record_id);
+
+
+--
+-- Name: schedules schedules_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT schedules_pkey PRIMARY KEY (schedule_id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: staffs staffs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.staffs
+    ADD CONSTRAINT staffs_pkey PRIMARY KEY (staff_id);
+
+
+--
+-- Name: messages messages_from_acc_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_from_acc_id_fkey FOREIGN KEY (from_acc_id) REFERENCES public.accounts(acc_id);
+
+
+--
+-- Name: messages messages_to_acc_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_to_acc_id_fkey FOREIGN KEY (to_acc_id) REFERENCES public.accounts(acc_id);
+
+
+--
+-- Name: patients patients_acc_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.patients
+    ADD CONSTRAINT patients_acc_id_fkey FOREIGN KEY (acc_id) REFERENCES public.accounts(acc_id);
+
+
+--
+-- Name: prescription_details prescription_details_prescription_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prescription_details
+    ADD CONSTRAINT prescription_details_prescription_id_fkey FOREIGN KEY (prescription_id) REFERENCES public.prescriptions(prescription_id);
+
+
+--
+-- Name: prescriptions prescriptions_record_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prescriptions
+    ADD CONSTRAINT prescriptions_record_id_fkey FOREIGN KEY (record_id) REFERENCES public.records(record_id);
+
+
+--
+-- Name: records records_doctor_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.records
+    ADD CONSTRAINT records_doctor_id_fkey FOREIGN KEY (doctor_id) REFERENCES public.staffs(staff_id);
+
+
+--
+-- Name: records records_patient_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.records
+    ADD CONSTRAINT records_patient_id_fkey FOREIGN KEY (patient_id) REFERENCES public.patients(patient_id);
+
+
+--
+-- Name: schedules schedules_patient_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schedules
+    ADD CONSTRAINT schedules_patient_id_fkey FOREIGN KEY (patient_id) REFERENCES public.patients(patient_id);
+
+
+--
+-- Name: staffs staffs_acc_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.staffs
+    ADD CONSTRAINT staffs_acc_id_fkey FOREIGN KEY (acc_id) REFERENCES public.accounts(acc_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+--
+-- Dbmate schema migrations
+--
+
+INSERT INTO public.schema_migrations (version) VALUES
+    ('20250501114344');
